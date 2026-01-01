7b682f76642ff455d3edf640f7f90ae23d20ec31
Jaypal Chauhan <jaypalchauhan@Jaypals-MacBook-Air.local>
2026-01-01 16:20:37 +0530

    Product Viewer Code Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero"
 import NavBar from "./components/Navbar"
+import ProductViewer from "./components/ProductViewer"
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Hero />
+      <ProductViewer />
     </main>
   )
 }
